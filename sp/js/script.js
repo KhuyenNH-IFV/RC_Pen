57b4e2a33b5fb7d3e0480ba54c56d985e13cd5e2
huukhuyen <nguyenhuukhuyenudn@gmail.com>
2017-09-12 09:28:21 +0700

    update mobile

--- sp/js/script.js
+++ sp/js/script.js
@@ -1,26 +1,17 @@
 $(function() {
-    // Paralax
-    $('body').parallax("50%", 0.1);
-    $('.paralax').parallax("85%", 0.4);
-    $('.paralax2').parallax("85%", 0.25);
-
     // Fade list image
     let offset_1 = $('.section-03').offset().top;
     let offset_2 = $('.section-04').offset().top;
-    let offset_3 = $('.section-07').offset().top;
     $(window).scroll(function(event) {
-        if ($(this).scrollTop() >= offset_1 - 50) {
+        if ($(this).scrollTop() >= offset_1) {
             $('.list_fade li').each(function(i) {
                 $(this).delay(400 * i).fadeIn(400);
             });
         }
-        if ($(this).scrollTop() >= offset_2 - 50) {
+        if ($(this).scrollTop() >= offset_2) {
             $('.list_fade2 li').each(function(i) {
                 $(this).delay(400 * i).fadeIn(400);
             });
-        }
-        if ($(this).scrollTop() >= offset_3 - 150) {
-            $('.fadeTop').addClass('ef-slide');
         }
     });
 
@@ -33,10 +24,4 @@ $(function() {
         return false;
     });
 
-    $('img').hover(function() {
-        $(this).attr('src', $(this).attr('src').replace('_off', '_on'));
-    }, function() {
-        $(this).attr('src', $(this).attr('src').replace('_on', '_off'));
-    });
-    
 });
